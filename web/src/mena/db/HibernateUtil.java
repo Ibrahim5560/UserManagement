38299ff49d9779a7e1471a02f107ad5538e22049
Ibrahim5560 <ibrahim5560.mmh@gmail.com>
2018-05-06 12:56:10 +0300

    Making  hibernate util with Genaeric Methods

--- web/src/mena/db/HibernateUtil.java
+++ web/src/mena/db/HibernateUtil.java
@@ -1,7 +1,13 @@
 package mena.db;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
 /**
  * Created by Ibrahim.mmh on 5/3/2018.
  */
@@ -20,5 +26,88 @@ public class HibernateUtil {
     }
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
-    }      
+    }
+    //---------------------------------hibernateTrx------------------------------
+    public static <E> void hibernateTrx(E line, String type, int id) // trx insert - update - delete
+    {
+        Session session= HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        try
+        {
+            tx = session.beginTransaction();
+            if(type.equals("insert"))
+                session.save(line);
+            if(type.equals("update"))
+            {
+                line = (E) session.get(line.getClass(), id);
+                if(line != null)
+                    session.update(line);
+            }
+            if(type.equals("delete"))
+            {
+                line = (E) session.get(line.getClass(), id);
+                if(line != null)
+                    session.delete(line);
+            }
+            tx.commit();
+        }
+        catch (HibernateException e)
+        {
+            if (tx!=null)
+                tx.rollback();
+            e.printStackTrace();
+        }
+        finally
+        {
+            session.close();
+        }
+    }
+    //---------------------------------getOneByID--------------------------------------
+    public static <E> E getOneByID (E lecturer,int id)  // select one record
+    {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        try
+        {
+            tx = session.beginTransaction();
+            lecturer = (E) session.get(lecturer.getClass(), id);
+            tx.commit();
+        }
+        catch (HibernateException e)
+        {
+            if (tx!=null)
+                tx.rollback();
+            e.printStackTrace();
+        }
+        finally
+        {
+            session.close();
+        }
+        return lecturer;
+    }
+    //---------------------------------getAllByHQL--------------------------------------
+    public static <E> List<E> getAllByHQL(String HQL)
+    {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        List<E>  lines = null;
+        try
+        {
+            tx = session.beginTransaction();
+            lines = (List<E>) session.createQuery(HQL).list();
+            tx.commit();
+        }
+        catch (HibernateException e)
+        {
+            if (tx!=null)
+                tx.rollback();
+            e.printStackTrace();
+        }
+        finally
+        {
+            session.close();
+        }
+        return lines;
+    }
+    //----------------------------------------------------------------------------------------
 }
